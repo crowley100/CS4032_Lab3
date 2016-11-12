eeac8c3dd31f167218260744fcda28b0076e7858
Lorcan <loreidy@tcd.ie>
2016-11-12 20:43:36 +0000

    starting work on creating a server environment

--- server.hs
+++ server.hs
@@ -7,8 +7,25 @@ import Network.Socket
 import Network.BSD
 import Control.Concurrent
 import Control.Monad
+import Data.List.Split
+import Data.List
+import Data.Map 
+import qualified Data.Map as Map
 
-myPool = 10
+
+myPool = 50
+
+data Client = Client
+    { clientID :: Int
+    , clientNick :: String
+    , clientConn :: Socket
+    }
+
+data Room = Room
+    { roomID :: Int
+    , roomName :: String
+    , roomClients :: [String]
+    } 
 
 threadPoolIO :: Int -> (a -> IO b) -> IO (Chan a, Chan b)
 threadPoolIO nr mutator = do
@@ -28,7 +45,17 @@ runServer port = do
     setSocketOption sock ReuseAddr 1           -- make socket reuseable
     bind sock (SockAddrInet (fromIntegral port) iNADDR_ANY)   -- listen on port.
     listen sock 3                             
-    (input,output) <- threadPoolIO myPool hdlConn   -- 5 worker threads waiting for connections
+    (input,output) <- threadPoolIO myPool hdlConn   -- 50 workers
+    -- testing
+    -- init maps
+    let rooms = empty
+        clients = empty
+    --    clients = Map empty
+    --    
+  
+    
+    --let str = roomName myRoom -- accessing elements of datatype
+    --print str
     mainLoop sock input port
   
 -- handle connections  
@@ -44,14 +71,22 @@ hdlConn (port,(sock, _)) = do
     t <- myThreadId
     print t
     handle <- socketToHandle sock ReadWriteMode
-    hSetBuffering handle LineBuffering
+    hSetBuffering handle NoBuffering -- Line??
     
-    msg <- hGetLine handle
+    msg <- hGetContents handle -- hGetLine ?? MAYBE ERROR HANDLING HERE!!! (see procress req)
+    let lines = words msg
     let hiMsg = myResponse msg "134.226.32.10" port
     print hiMsg
-    case head $ words msg of
+    
+    -- watch for spaces in parsing (e.g. in join param 1(2))
+    -- error handling here: accepted <- try (handler params) case accepted of Left error Right finish
+    case head lines of
         "KILL_SERVICE" -> sClose sock
         "HELO" -> hPutStr handle hiMsg
+        --"DISCONNECT:" -> clientDisc handle (splitColon $ lines!!2)
+        --"JOIN_CHATROOM:" -> clientJoin handle (splitColon $ head lines) (splitColon $ lines!!3)
+        --"LEAVE_CHATROOM:" -> clientLeave handle (read $ splitColon $ head lines) (splitColon $ lines!!1) (splitColon $ lines!!2) --nother read??
+        --"CHAT:" -> clientChat (read $ splitColon $ head lines) (splitColon $ lines!!2) (splitColon $ lines!!3)
         _ -> putStrLn $ "Unknown Message:" ++ msg
     
     hClose handle
@@ -61,6 +96,14 @@ myResponse msg host port = msg ++ "\n" ++
                            "IP:" ++ host ++ "\n" ++
                            "Port:" ++ show port ++ "\n" ++
                            "StudentID:13333179"  
+     
+-- syntax?                      
+splitColon :: String -> String
+splitColon str = (splitOn ":" str) !! 1
+
+-- WORKING HERE: LOOK AT HOW TO SET UP AN ENVIRONMENT TO ACCESS MAPS
+--clientJoin :: Handle -> String -> String -> IO () -- return type?
+--clientJoin handle roomName clientName
     
 main :: IO ()
 main = withSocketsDo $ do
